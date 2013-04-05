0e692a7a7ee3f99e8086c5f47c194fbd944920b9
Reid Vandewiele <reid@puppetlabs.com>
2013-04-04 22:05:08 -0700

    Fix swapped Debian/RedHat params

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,14 +3,14 @@ class tomcat::params {
   case $::osfamily {
     'RedHat': {
       $tomcat_package = 'tomcat6'
-      $admin_package  = 'tomcat6-admin'
-      $docs_package   = 'tomcat6-docs'
+      $admin_package  = 'tomcat6-admin-webapps'
+      $docs_package   = 'tomcat6-docs-webapp'
       $service        = 'tomcat6'
     }
     'Debian': {
       $tomcat_package = 'tomcat6'
-      $admin_package  = 'tomcat6-admin-webapps'
-      $docs_package   = 'tomcat6-docs-webapp'
+      $admin_package  = 'tomcat6-admin'
+      $docs_package   = 'tomcat6-docs'
       $service        = 'tomcat6'
     }
   }
