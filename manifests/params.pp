c5d05882b4df255fc81167aa7bdaabaa1cb9152c
Reid Vandewiele <reid@puppetlabs.com>
2013-05-03 17:07:26 +0100

    Update Ubuntu support and bumb version
    This is mostly just a params file update to add support for another
    platform

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,20 +3,26 @@ class tomcat::params {
   case $::osfamily {
     default: { fail("unsupported OS: ${::osfamily}") }
     'RedHat': {
-      $tomcat_package  = 'tomcat6'
       $admin_package   = 'tomcat6-admin-webapps'
-      $docs_package    = 'tomcat6-docs-webapp'
-      $service         = 'tomcat6'
       $autodeploy_dir  = '/var/lib/tomcat6/webapps'
+      $docs_package    = 'tomcat6-docs-webapp'
+      $group           = 'tomcat'
+      $service         = 'tomcat6'
       $staging_dir     = '/var/lib/tomcat6/staging'
+      $tomcat_package  = 'tomcat6'
+      $user            = 'tomcat'
+      $user_homedir    = '/usr/share/tomcat6'
     }
     'Debian': {
-      $tomcat_package = 'tomcat6'
-      $admin_package  = 'tomcat6-admin'
-      $docs_package   = 'tomcat6-docs'
-      $service        = 'tomcat6'
-      #$autodeploy_dir  = '/var/lib/tomcat6/webapps'
-      #$staging_dir     = '/var/lib/tomcat6/staging'
+      $admin_package   = 'tomcat6-admin'
+      $autodeploy_dir  = '/var/lib/tomcat6/webapps'
+      $docs_package    = 'tomcat6-docs'
+      $group           = 'tomcat6'
+      $service         = 'tomcat6'
+      $staging_dir     = '/var/lib/tomcat6/staging'
+      $tomcat_package  = 'tomcat6'
+      $user            = 'tomcat6'
+      $user_homedir    = '/usr/share/tomcat6'
     }
   }
 
